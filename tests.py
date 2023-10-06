714f67e109bf5b6eba6d9a8ad3e09fa42121f7d1
Your Name <you@example.com>
2023-10-06 17:40:44 +0300

    Добавил ReadMe

--- tests.py
+++ tests.py
@@ -3,14 +3,20 @@
 def main():
     values = get_values()
     unknown = int(input("Сколько неправильных?: "))
-    #target = float(input("Сумма: "))
-    target = 451643567
+    target = float(input("Сумма: "))
+    #target = 451643567
     slack = 10
     print(values)
-    combos = find_combo([], unknown, 1, values, target, slack)
+    combos = []
+    find_combo([], unknown, 1, values, target, slack, combos)
     print()
-    for i in combos:
-        print(i)
+    if not combos:
+        print("Нет совпадений")
+        return
+    for combo in combos:
+        for element in combo:
+            print(f"{values[element]} \t", end="")
+        print(f" = {sum([values[i] for i in combo])}")
 
 
 def get_values():
@@ -20,11 +26,12 @@ def get_values():
         for row in file.readlines():
             row = row.strip('\n').replace(",", ".")
             values.append(float(row))
+    if not values:
+        raise ValueError("Нет значений в файлике 'numbers.txt' или самого файлика ")
     return values
 
 
-def find_combo(indices: list, limit: int, cut_depth: int, values: list, trgt: float, slack):
-    combo = []
+def find_combo(indices: list, limit: int, cut_depth: int, values: list, trgt: float, slack, combos:list):
     for i in values:
         index = values.index(i)
         if index in indices:
@@ -36,15 +43,15 @@ def find_combo(indices: list, limit: int, cut_depth: int, values: list, trgt: fl
             s = sum(values) - sum([values[i] for i in indices])
             #print(s)
             if s < trgt + slack and s > trgt - slack:
-                combo.append(indices.copy())
+                combos.append(indices.copy())
                 print(indices, "got it------------------------------")
             indices.remove(index)
         else:
             indices.append(index)
             #print(indices, "skip")
-            combo.append(find_combo(indices, limit, cut_depth + 1, values, trgt, slack))
+            some_combo = find_combo(indices, limit, cut_depth + 1, values, trgt, slack, combos)
             indices.remove(index)
-    return combo
 
-
-main()
+if __name__ == "__main__":
+    while True:
+        main()
